fc96de2e31cb02452849ac98c7c8bf2775577517
ShriRadhakrishnan <shriharran@gmail.com>
2024-06-09 21:11:52 -0400

    First Commit

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
-  
+import './About.css';
+import pfp from '../assets/pfp.jpeg'; 
+
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+        <div className="split left">
+          <div className="centered">
+            <img
+              className="profile_image"
+              src={pfp}
+              alt="Profile Pic"
+            />
+          </div>
+        </div>
+        <div className="split right">
+          <div className="centered">
+            <div className="name_title">Shri Radha</div>
+            <div className="brief_description">
+            Hi! I'm Shriharran Radhakrishnan but I go by Shri. I am a rising junior at UMass Amherst studying Computer Science and Math. One of my hobbies is making music and playing drums.
+            </div>
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
